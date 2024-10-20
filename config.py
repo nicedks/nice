259ccaaa7838989f05fe52d5d585dc9ee6e9a4f1
nicedks <demonnicedks@gmail.com>
2024-10-20 18:11:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sy465143:B7UENblZjA3y3xAG>@cluster0.xeac9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "Premiumkorn")
+DB_NAME = os.environ.get("DATABASE_NAME", "Premium-corn")
 
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", "-1002389611214"))
